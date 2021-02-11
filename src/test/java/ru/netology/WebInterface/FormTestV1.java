3d682853b5b16dacbde997c99dc8f0ec30156f0e
Aleksandra Zhukova <a.hahina@gmail.com>
2021-02-11 13:59:46 +0300

    some fixes

--- src/test/java/ru/netology/WebInterface/FormTestV1.java
+++ src/test/java/ru/netology/WebInterface/FormTestV1.java
@@ -1,4 +1,4 @@
-package ru.netology.WebInterface;
+package ru.netology.webinterface;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.AfterEach;
@@ -12,51 +12,59 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 
+import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FormTestV1 {
-    @Test
-    void TestCorrect() {
+    @BeforeEach
+    void setUp() {
         open("http://localhost:9999");
-        SelenideElement form = $("form");
-        form.$("[data-test-id=name] input").setValue("Василий Петрович-Репин");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
-    void TestBadName() {
-        open("http://localhost:9999");
+    void testCorrect() {
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий Петров-Водкин");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText(expected));
+    }
+
+    @Test
+    void testBadName() {
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
         SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Vasya");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $("[data-test-id=order-success]").shouldNotHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=name]").shouldHave(cssClass("input_invalid"));
+        $("[data-test-id=name].input_invalid .input__inner .input__sub").shouldHave(exactText(expected));
     }
 
     @Test
-    void TestBadPhone() {
-        open("http://localhost:9999");
+    void testBadPhone() {
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
         SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+7927000000");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        $("[data-test-id=order-success]").shouldNotHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=phone]").shouldHave(cssClass("input_invalid"));
+        $("[data-test-id=phone].input_invalid .input__inner .input__sub").shouldHave(exactText(expected));
     }
 
     @Test
-    void TestBadAgreement() {
-        open("http://localhost:9999");
+    void testBadAgreement() {
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
         SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("button").click();
-        $("[data-test-id=order-success]").shouldNotHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=agreement]").shouldHave(cssClass("input_invalid"));
     }
 }
